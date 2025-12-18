41bb3c4b6b25324e7e301234ff19443c3e8049fe
M. Yousaf Ali Shah <yousafshahyusaf@gmail.com>
2025-12-18 09:30:07 +0500

    date formate udpated

--- src/components/reportsComponent/energyComparisonTable/HarmonicAnalytics.jsx
+++ src/components/reportsComponent/energyComparisonTable/HarmonicAnalytics.jsx
@@ -95,7 +95,7 @@ const HarmonicAnalytics = ({
     const hours = String(d.getHours()).padStart(2, "0");
     const minutes = String(d.getMinutes()).padStart(2, "0");
 
-    return `/${year}${month}/${day} - ${hours}:${minutes}`;
+    return `${year}/${month}/${day} - ${hours}:${minutes}`;
   };
 
   //=======================================PDF export start ================================
